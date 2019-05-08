213940382ce3fae304fb6b0f4bfdbd23627804b6
Mateusz <wietrzykmateusz@gmail.com>
2019-05-09 00:59:06 +0200

    scrolling

--- js/script.js
+++ js/script.js
@@ -7,5 +7,17 @@ hamburger.addEventListener("click", function() {
      menu.classList.toggle("hide");
 })
 
+//---------------------------GALLERY---------------------------
 
+function myFunction(imgs) {
+     var expandImg = document.getElementById("expandedImg");
+     var imgText = document.getElementById("imgtext");
+     expandImg.src = imgs.src;
+     
+     imgText.innerHTML = imgs.alt;
+
+     expandImg.parentElement.style.display = "block";
+   } 
+
+//----------------------------------Scrolling to part---------------------
 
